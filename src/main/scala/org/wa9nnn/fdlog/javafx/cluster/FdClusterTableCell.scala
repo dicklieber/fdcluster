9d7feb8d9ddea3de574633cab058eef8faaaddb8
dlieber <dick@u505.com>
2019-09-18 16:22:50 -0500

    Multiline tooltip for QsoHourDigest cells.

--- src/main/scala/org/wa9nnn/fdlog/javafx/cluster/FdClusterTableCell.scala
+++ src/main/scala/org/wa9nnn/fdlog/javafx/cluster/FdClusterTableCell.scala
@@ -6,27 +6,36 @@ import java.time.LocalDateTime
 import com.typesafe.scalalogging.LazyLogging
 import org.wa9nnn.fdlog.model.TimeFormat.formatLocalDateTime
 import org.wa9nnn.fdlog.model.sync.QsoHourDigest
+import org.wa9nnn.fdlog.store.network.FdHour
 import scalafx.scene.control.TableCell
 
 class FdClusterTableCell[S, T] extends TableCell[S, T] with LazyLogging {
-  item.onChange { (v, j1, j2) =>
-    val value = v.value
-    text = j2.toString
+  item.onChange { (v, oldValue, newValue) =>
+    Option(newValue).foreach{v ⇒
 
-    try {
-      val r = value
-      val rendered :String= r match {
-        case qhd: QsoHourDigest ⇒
-          tooltip = s"${qhd.size} QSOs digest: ${qhd.digest}"
-          val truncated = new String(qhd.digest.take(10))
-          s"${qhd.size} $truncated..."
-        case stamp: LocalDateTime ⇒ stamp
-        case string: String ⇒ string
-        case other ⇒ other.toString
+      try {
+        val rendered :String= v match {
+          case qhd: QsoHourDigest ⇒
+            tooltip =
+              s"""Day:\t${qhd.startOfHour.day}
+                 |Hour:\t${qhd.startOfHour.hour}
+                 |QSOs:\t${qhd.size}
+                 |digest:\t${qhd.digest}
+                 Digest is a checksum of all the QSO's UUIDs for the hour.""".stripMargin
+            val truncated = new String(qhd.digest.take(10))
+            s"${qhd.size} $truncated..."
+          case fdHour:FdHour ⇒
+            tooltip = s"Day of month: ${fdHour.day} hour: ${fdHour.hour}"
+            fdHour.toString
+          case stamp: LocalDateTime ⇒ stamp
+          case string: String ⇒ string
+          case other ⇒ other.toString
+        }
+        text = rendered
+      } catch {
+        case x: Throwable ⇒ x.printStackTrace()
       }
-      text = rendered
-    } catch {
-      case x: Throwable ⇒ x.printStackTrace()
+
     }
 
   }
