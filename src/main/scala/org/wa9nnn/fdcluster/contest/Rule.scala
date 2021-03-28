ba4243d7860610e67e33c0e46e33e867e9e012bb
dlieber <dick@u505.com>
2021-03-28 14:13:33 -0500

    add contest logos.

---
+++ src/main/scala/org/wa9nnn/fdcluster/contest/Rule.scala
@@ -0,0 +1,8 @@
+
+package org.wa9nnn.fdcluster.contest
+
+class Rule(name:String, description:String, pointsPerN:Int, n:Option[Int] = None, dependentCategories:Seq[String]= Seq.empty) {
+
+  "100% emergency: "
+}
+
---
+++ src/test/scala/org/wa9nnn/fdcluster/contest/RuleSpecs.scala
@@ -0,0 +1,9 @@
+package org.wa9nnn.fdcluster.contest
+
+import org.specs2.mutable.Specification
+
+class RuleSpecs extends Specification {
+  "Rule" >> {
+
+  }
+}
