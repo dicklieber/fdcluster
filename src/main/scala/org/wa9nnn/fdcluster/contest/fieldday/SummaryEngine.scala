2dfc7d3b4749c39c08fd682e5e1724a92079869e
dlieber <dick@u505.com>
2021-04-01 22:31:18 -0500

    1st try with util library

--- src/main/scala/org/wa9nnn/fdcluster/contest/fieldday/SummaryEngine.scala
+++ src/main/scala/org/wa9nnn/fdcluster/contest/fieldday/SummaryEngine.scala
@@ -2,6 +2,7 @@
 package org.wa9nnn.fdcluster.contest.fieldday
 
 import com.google.inject.name.Named
+import com.wa9nnn.util.tableui.{Cell, Table}
 import org.wa9nnn.fdcluster.contest.Contest
 import org.wa9nnn.fdcluster.model.{AllContestRules, EntryCategories, EntryCategory, QsoRecord}
 import play.twirl.api.HtmlFormat
@@ -13,16 +14,22 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 //@Named("allQsos") allQsos: ObservableBuffer[QsoRecord]
 class SummaryEngine @Inject()(allContestRules: AllContestRules) {
-  def apply(writer: Writer, contest: Contest, wfd:WinterFieldDaySettings): Unit = {
+  def apply(writer: Writer, contest: Contest, wfd: WinterFieldDaySettings): Unit = {
     val contestRules = allContestRules.byContestName(contest.eventName)
     val categories: Seq[EntryCategory] = contestRules.categories.categories.toSeq
 
+    val rows = Seq ()
+    val bandModeBreakDown = Table(Seq(
+      Seq("", Cell("CW", colSpan = 2), Cell("Digital", colSpan = 2), Cell("Phone", colSpan = 2)),
+      Seq("Band", "QSOs", "Pwr(W)", "QSOs", "Pwr(W)", "QSOs", "Pwr(W)")
+    ), rows)
 
-
+    val appendable1: HtmlFormat.Appendable = com.wa9nnn.util.tableui.html.renderTable(bandModeBreakDown)
+    val bandModeetc = appendable1.toString()
     val appendable: HtmlFormat.Appendable = html.FieldDaySummary(contest, wfd, categories)
 
     val contentType = appendable.contentType
     val str = appendable.toString()
-    writer.write(str)
+    writer.write(str + bandModeetc)
   }
 }
